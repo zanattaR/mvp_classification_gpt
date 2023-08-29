eba553624bb537142e9ccfd8e755b46e97515ae0
Giovanni Zanatta <giovanni.zanatta@rankmyapp.com>
2023-08-29 16:24:14 -0300

    increase sleep to 7 segs

--- mvp_classification_gpt.py
+++ mvp_classification_gpt.py
@@ -82,14 +82,14 @@ if st.button('Gerar Classificações'):
 
     # Request na API p/ gerar classificações
     results_detail = asyncio.run(get_chatgpt_responses(system=system_detail, lotes_reviews=lotes_reviews))
-    time.sleep(3)
+    time.sleep(7)
 
     results_sentiment = asyncio.run(get_chatgpt_responses(system=system_sentiment, lotes_reviews=lotes_reviews))
-    time.sleep(3)
+    time.sleep(7)
 
     results_subcategory = asyncio.run(get_chatgpt_responses(system=system_subcategory, lotes_reviews=lotes_reviews))
-    time.sleep(3)
-    
+    time.sleep(7)
+
     results_category = asyncio.run(get_chatgpt_responses(system=system_category, lotes_reviews=lotes_reviews))
     
 
