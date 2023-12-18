fb8bb5c5cbbb00d51e66daffa7d631897db20490
zanattaR <67986361+zanattaR@users.noreply.github.com>
2023-12-18 17:57:17 -0300

    test_data_cleaning

--- mvp_classification_gpt.py
+++ mvp_classification_gpt.py
@@ -115,10 +115,10 @@ if st.button('Gerar Classificações'):
     df_results_subcategory = normalize_results(results_subcategory)
     df_results_detail = normalize_results(results_detail)
     
-    df_results_sentiment.dropna(inplace=True, axis=0)
-    df_results_category.dropna(inplace=True, axis=0)
-    df_results_subcategory.dropna(inplace=True, axis=0)
-    df_results_detail.dropna(inplace=True, axis=0)
+    #df_results_sentiment.dropna(inplace=True, axis=0)
+    #df_results_category.dropna(inplace=True, axis=0)
+    #df_results_subcategory.dropna(inplace=True, axis=0)
+    #df_results_detail.dropna(inplace=True, axis=0)
 
     st.write(df_results_sentiment)
     st.write(df_results_category)
@@ -130,6 +130,12 @@ if st.button('Gerar Classificações'):
     df_results_category = clean_results(df_results_category)
     df_results_subcategory = clean_results(df_results_subcategory)
     df_results_detail = clean_results(df_results_detail)
+
+    st.write('after clean')
+    st.write(df_results_sentiment)
+    st.write(df_results_category)
+    st.write(df_results_subcategory)
+    st.write(df_results_detail)
 
     # Acrescentar classificações no df de reviews, renomear colunas, adicionar valor Genérico caso não venha classificação da API
     df_reviews_sentiment = format_results(df_reviews=df_reviews, df_results=df_results_sentiment, group="Sentiment")
